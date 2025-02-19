8d2bc886f4361436820e26b7664fe7e9aa5f3859
Ranjit jana <jranjit367@gmail.com>
2025-02-19 15:57:04 +0530

    fix env

--- api/index.js
+++ api/index.js
@@ -11,11 +11,4 @@ mongoose.connect(process.env.MONGO).then(()=>{
     console.log(err);
 })
 
-
-
-
-
-
-app.listen(3000, ()=>{
-    console.log('server running on port 3000')
-});
+app.listen(3000);
